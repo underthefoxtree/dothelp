581928b715c0b3435b33f1e1ec0ad76dd46e6616
underthefoxtree <l.vondrenkmann@yahoo.com>
2023-04-28 16:56:25 +0100

    Rework option input to optimize

--- src/complexBuildModel.go
+++ src/complexBuildModel.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"time"
-
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type option struct {
@@ -15,24 +12,20 @@ type option struct {
 }
 
 type complexBuildModel struct {
-	options []option
-	cursor  int
-	state   int
-	list    tea.Model
-	input   textinput.Model
-	ch      chan string
+	options    []option
+	cursor     int
+	optionBool bool
+	state      int
+	input      textinput.Model
 }
 
 func createComplexBuildModel() (tea.Model, tea.Cmd) {
-	ch := make(chan string)
-
 	return complexBuildModel{
 		options: []option{
 			{name: "Architecture", otype: 1},
 			{name: "Force", otype: 0},
 			{name: "Exit", otype: 2},
 		},
-		ch: ch,
 	}, nil
 }
 
@@ -41,111 +34,94 @@ func (m complexBuildModel) Init() tea.Cmd {
 }
 
 func (m complexBuildModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	select {
-	case in := <-m.ch:
-		switch m.options[m.cursor].otype {
+	switch m.state {
+	case 0:
+		switch msg := msg.(type) {
 
-		case 0:
-			m.options[m.cursor].value = in
-			m.state = 0
-			return m, nil
+		case tea.KeyMsg:
 
-		default:
-			return m, tea.Quit
+			switch msg.String() {
+
+			case "ctrl+c", "q":
+				return createExitModel("Exiting...")
+
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			case "down", "j":
+				if m.cursor < len(m.options)-1 {
+					m.cursor++
+				}
+
+			case "enter":
+				switch m.options[m.cursor].otype {
+				case 0:
+					m.state = 1
+					m.optionBool = true
+
+				case 1:
+					m.state = 2
+					m.input = textinput.New()
+					m.input.Placeholder = "Default"
+					m.input.Focus()
+					return m, textinput.Blink
+
+				default:
+					return createExitModel("Exiting...")
+				}
+			}
 		}
+
+		return m, nil
+
+	case 1:
+		switch msg := msg.(type) {
+
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "up", "k", "down", "j":
+				m.optionBool = !m.optionBool
+			case "enter":
+				m.state = 0
+				if m.optionBool {
+					m.options[m.cursor].value = "Yes"
+				} else {
+					m.options[m.cursor].value = "No"
+				}
+			}
+		}
+
+		return m, nil
+
+	case 2:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "ctrl+c", "q":
+				return createExitModel("Exiting...")
+			case "enter":
+				m.state = 0
+				m.options[m.cursor].value = m.input.Value()
+				return m, nil
+			}
+		}
+
+		var cmd tea.Cmd
+		m.input, cmd = m.input.Update(msg)
+		return m, cmd
 
 	default:
-		switch m.state {
-		case 0:
-			switch msg := msg.(type) {
-
-			case tea.KeyMsg:
-
-				switch msg.String() {
-
-				case "ctrl+c", "q":
-					return createExitModel("Exiting...")
-
-				case "up", "k":
-					if m.cursor > 0 {
-						m.cursor--
-					}
-
-				case "down", "j":
-					if m.cursor < len(m.options)-1 {
-						m.cursor++
-					}
-
-				case "enter":
-					m.state = 1
-					switch m.options[m.cursor].otype {
-					case 0:
-						m.list = listModel{
-							options: []string{
-								"Yes",
-								"No",
-							},
-							getOptionStyle: func(option string) lipgloss.Style {
-								return selectedItemStyle
-							},
-							selectOption: func(option string) (tea.Model, tea.Cmd) {
-								go func() {
-									m.ch <- option
-								}()
-								return m, tea.Every(time.Second, func(t time.Time) tea.Msg {
-									return t
-								})
-							},
-						}
-					case 1:
-						m.input = textinput.New()
-						m.input.Placeholder = "Default"
-						m.input.Focus()
-						return m, textinput.Blink
-
-					default:
-						return createExitModel("Exiting...")
-					}
-				}
-			}
-
-			return m, nil
-
-		default:
-			switch m.options[m.cursor].otype {
-
-			case 0:
-				var cmd tea.Cmd
-				m.list, cmd = m.list.Update(msg)
-				return m, cmd
-
-			case 1:
-				switch msg := msg.(type) {
-				case tea.KeyMsg:
-					switch msg.String() {
-					case "enter":
-						m.options[m.cursor].value = m.input.Value()
-						m.state = 0
-						return m, nil
-					}
-				}
-
-				var cmd tea.Cmd
-				m.input, cmd = m.input.Update(msg)
-				return m, cmd
-
-			default:
-				return createExitModel("Exiting...")
-			}
-		}
+		return createExitModel("Exit")
 	}
 }
 
 func (m complexBuildModel) View() string {
+	s := titleStyle.Render("DOTHELP") + "\n\n"
+
 	switch m.state {
 	case 0:
-		s := titleStyle.Render("DOTHELP") + "\n\n"
-
 		for i, choice := range m.options {
 			cursor := "  "
 			style := itemStyle
@@ -164,34 +140,29 @@ func (m complexBuildModel) View() string {
 				if choice.value != "" {
 					v = choice.value
 				}
-				s += cursor + style.Render(choice.name, ":", v) + "\n"
+				s += cursor + style.Render(choice.name+":", v) + "\n"
 			case 1:
 				v := "Default"
 				if choice.value != "" {
 					v = choice.value
 				}
-				s += cursor + style.Render(choice.name, ":", v) + "\n"
+				s += cursor + style.Render(choice.name+":", v) + "\n"
 			case 2:
 				s += cursor + style.Render(choice.name) + "\n"
 			}
 		}
 
-		s += helpStyle.Render("\nPress q to quit.")
-
-		return s
-
-	default:
-		switch m.options[m.cursor].otype {
-		case 0:
-			return m.list.View()
-		default:
-			s := titleStyle.Render("DOTHELP") + "\n\n"
-
-			s += "Set " + m.options[m.cursor].name + " (empty for default):\n"
-			s += m.input.View()
-			s += helpStyle.Render("\n\nPress q to quit")
-
-			return s
+	case 1:
+		if m.optionBool {
+			s += "> " + selectedItemStyle.Render("Yes") + "\n  " + itemStyle.Render("No")
+		} else {
+			s += "  " + itemStyle.Render("Yes") + "\n> " + selectedItemStyle.Render("No")
 		}
+
+	case 2:
+		s += m.input.View()
 	}
+
+	s += helpStyle.Render("\nPress q to quit.")
+	return s
 }
